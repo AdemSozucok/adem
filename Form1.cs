1734deb961741e489a27cc718d01eb7fa40f9b1b
AdemSozucok <100554124+AdemSozucok@users.noreply.github.com>
2022-03-07 11:13:01 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace adem
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+
+        private void bnliste_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sehirler.Count; i++)
+            {
+                lbListe.Items.Add(sehirler[i]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        ArrayList sehirler = new ArrayList();
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            sehirler.Add(txtSehirAdi.Text);
+        }
+    }
+}
+    
+
